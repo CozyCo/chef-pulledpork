522bb84295f1df93a9706d7a8bc9444317d887e8
Matt Greensmith <matt.greensmith@gmail.com>
2016-01-20 11:04:09 -0800

    remove one-time run from default

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,14 +20,5 @@
 include_recipe 'pulledpork::install'
 include_recipe 'pulledpork::configure'
 
-# one time pulled pork run for first install / config changes
-bash 'run_pulledpork' do
-  code <<-EOH
-  /usr/local/bin/pulledpork.pl -c #{node['pulledpork']['pp_config_path']} -l;
-  service #{node['pulledpork']['snort_svc_name']} restart
-  EOH
-  action :nothing
-end
-
 # This is really just a cron job
 include_recipe 'pulledpork::service'
